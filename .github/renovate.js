e3335a5afcb6d128c38a7e9bc7e2b941778d82ab
Aurifi <dennis.stieglitz@web.de>
2026-01-10 16:50:42 +0100

    ci: add renovate pipeline, closes #5

---
+++ .github/renovate.js
@@ -0,0 +1,7 @@
+module.exports = {
+  branchPrefix: "renovate/",
+  username: "renovate-bot",
+  gitAuthor: "Renovate Bot <bot@renovateapp.com>",
+  platform: "github",
+  repositories: ["Aurifi/sbomreport-to-dependencytrack"],
+};
